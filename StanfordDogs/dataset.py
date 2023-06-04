56715e15b8562b4e510ad2f10ef6df3ecd65a285
kimye702 <kimye702@naver.com>
2023-06-04 15:05:39 +0900

    z

--- StanfordDogs/dataset.py
+++ StanfordDogs/dataset.py
@@ -1,31 +1,21 @@
-class StanfordDataset(Dataset):
-    def __init__(self, src, resol, transform=None):
-        self.normalize = transforms.Normalize(
-            mean=[0.5, 0.5, 0.5],
-            std=[0.5, 0.5, 0.5]
-        )
+from torchvision.datasets import ImageFolder
 
-        self.transform = transform
-
-        self.target_transform = transforms.Compose([
-            transforms.Lambda(toTensor),
-            transforms.Lambda(toOne_hot),
-        ])
-
-        self.dataset = ImageFolder(
+class StanfordDataset(ImageFolder):
+    def __init__(self, src, resol, transform=None, target_transform=None):
+        super().__init__(
             src,
-            transform=self.transform,
-            target_transform=self.target_transform
+            transform=transform,
+            target_transform=target_transform
         )
+
+    def __getitem__(self, idx):
+        image, label = super().__getitem__(idx)
+        
+        if self.transform is not None:
+            image = self.transform(image)
+        
+        return image, label
 
     def __len__(self):
         return len(self.dataset)
 
-    def __getitem__(self, idx):
-        image = self.dataset[idx][0]
-        label = self.dataset[idx][1]
-
-        if self.transform is not None:
-            image = self.transform(image)
-
-        return image, label
