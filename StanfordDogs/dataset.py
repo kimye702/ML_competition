8fca39085d1119c2208ab35fea29f1f921a47796
kimye702 <kimye702@naver.com>
2023-06-04 15:08:01 +0900

    ㅈㅂ

--- StanfordDogs/dataset.py
+++ StanfordDogs/dataset.py
@@ -1,4 +1,8 @@
+import torch
+import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class StanfordDataset(ImageFolder):
     def __init__(self, src, resol, transform=None, target_transform=None):
@@ -10,10 +14,10 @@ class StanfordDataset(ImageFolder):
 
     def __getitem__(self, idx):
         image, label = super().__getitem__(idx)
-        
+
         if self.transform is not None:
             image = self.transform(image)
-        
+
         return image, label
 
     def __len__(self):
